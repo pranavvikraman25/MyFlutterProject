aa6cb42905911f7beba8e78eeee9c8a81899bc1c
Pranav Vikraman s s <pranav.vikraman.25@gmail.com>
2025-11-18 14:26:28 +0530

    Update task_tile.dart

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,1 +1,35 @@
-ok
+import 'package:flutter/material.dart';
+import '../models/task_model.dart';
+
+class TaskTile extends StatelessWidget {
+  final TaskModel task;
+  final Function(bool?) onChanged;
+  final Function() onDelete;
+
+  const TaskTile({
+    super.key,
+    required this.task,
+    required this.onChanged,
+    required this.onDelete,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Checkbox(
+        value: task.isDone,
+        onChanged: onChanged,
+      ),
+      title: Text(
+        task.title,
+        style: TextStyle(
+          decoration: task.isDone ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: IconButton(
+        icon: const Icon(Icons.delete),
+        onPressed: onDelete,
+      ),
+    );
+  }
+}
