1bc97d5ea940a0e4dafe44fb84e863d75ff9f7f7
Pranav Vikraman s s <pranav.vikraman.25@gmail.com>
2025-11-18 14:25:10 +0530

    Update task_model.dart

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,1 +1,14 @@
-ok
+import 'package:hive/hive.dart';
+
+part 'task_model.g.dart';
+
+@HiveType(typeId: 0)
+class TaskModel {
+  @HiveField(0)
+  String title;
+
+  @HiveField(1)
+  bool isDone;
+
+  TaskModel({required this.title, this.isDone = false});
+}
